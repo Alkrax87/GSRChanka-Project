ea36f1482cec4b9d9d2f4710ae8512ede7a864ca
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-18 10:19:30 -0500

    Preparación de librerías base para el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FontAwesomeModule],
+  template: `
+    <h1 class="text-green-700 text-3xl font-bold underline">
+      <fa-icon [icon]="Gear"></fa-icon> {{ title }}
+    </h1>
+    <router-outlet></router-outlet>
+  `,
+  styles: ``,
 })
 export class AppComponent {
+  Gear = faGear;
   title = 'GSRChanka-Project';
 }
