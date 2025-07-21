32a9ad288caea69d8ff968c090d36c69ec8ad95a
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-21 14:30:00 -0500

    Actualización de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/portal/main/main.component';
+import { HomeComponent } from './pages/portal/home/home.component';
+import { UsuarioComponent } from './pages/portal/usuario/usuario.component';
+import { LandingComponent } from './pages/public/landing/landing.component';
+import { LoginComponent } from './pages/public/login/login.component';
+import { TramitesComponent } from './pages/public/tramites/tramites.component';
+import { SeguimientoComponent } from './pages/public/seguimiento/seguimiento.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', component: LandingComponent },
+  { path: 'tramite', component: TramitesComponent },
+  { path: 'seguimiento', component: SeguimientoComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'portal',
+    component: MainComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'usuario', component: UsuarioComponent },
+    ],
+  },
 ];
