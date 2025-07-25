13a2d8e9fb5a5b9041db1a4439be7cda5dfed667
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-24 20:41:12 -0500

    Test simple para logOut

--- src/app/pages/portal/main/main.component.ts
+++ src/app/pages/portal/main/main.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      main works!
-    </p>
+    <div class="w-full h-screen flex items-center justify-center">
+      <button (click)="logOut()" class="bg-red-600 hover:bg-opacity-90 text-white border rounded-full px-4 py-2 outline-none">
+        Cerrar Sesión
+      </button>
+    </div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class MainComponent {
+  constructor(private authService: AuthService) {}
 
+  logOut() {
+    this.authService.logOut();
+  }
 }
