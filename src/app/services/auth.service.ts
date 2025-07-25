788ba540dae7b78716768333ec28ae9ce1017867
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-07-24 20:39:28 -0500

    Se implementó funciones de login y logOut

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword } from "@angular/fire/auth";
+import { Auth, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "@angular/fire/auth";
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: Auth) {}
+  constructor(private auth: Auth, private router: Router) {}
 
   async registerUser(email: string, password: string) {
+    const auth = getAuth();
     try {
-      await createUserWithEmailAndPassword(this.auth, email, password);
+      await createUserWithEmailAndPassword(auth, email, password);
     } catch (error) {
       throw error;
+    }
+  }
+
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email + '@gsrchanka.com', password);
+  }
+
+  async logOut() {
+    const auth = getAuth();
+    try {
+      await signOut(auth);
+
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.log(error);
     }
   }
 }
