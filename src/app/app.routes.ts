f2b312559e02e34232fc2059b63356605837635e
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-01 14:25:28 -0500

    Se agregó guard para protejer las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { MainComponent } from './pages/portal/main/main.component';
 import { HomeComponent } from './pages/portal/home/home.component';
 import { UsuarioComponent } from './pages/portal/usuario/usuario.component';
@@ -7,17 +8,28 @@ import { LoginComponent } from './pages/public/login/login.component';
 import { TramitesComponent } from './pages/public/tramites/tramites.component';
 import { SeguimientoComponent } from './pages/public/seguimiento/seguimiento.component';
 
+const redirectLoggedIn = () => redirectLoggedInTo(['portal/home']);
+const redirectUnauthorizedUser = () => redirectUnauthorizedTo(['login']);
+
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'tramite', component: TramitesComponent },
   { path: 'seguimiento', component: SeguimientoComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedIn },
+  },
   {
     path: 'portal',
     component: MainComponent,
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedUser },
     children: [
       { path: 'home', component: HomeComponent },
       { path: 'usuario', component: UsuarioComponent },
     ],
   },
+  { path: '**', redirectTo: '' },
 ];
