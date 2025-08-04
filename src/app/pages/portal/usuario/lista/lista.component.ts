271de6ff3181763168382e2aefc552210aebaf95
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-04 15:54:09 -0500

    Preparación de componentes de usuario

---
+++ src/app/pages/portal/usuario/lista/lista.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista',
+  imports: [],
+  template: `
+    <p>
+      lista works!
+    </p>
+  `,
+  styles: ``
+})
+export class ListaComponent {
+
+}
--- src/app/pages/portal/usuario/usuario.component.ts
+++ src/app/pages/portal/usuario/usuario.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [RouterOutlet],
   template: `
     <p>
       usuario works!
     </p>
+    <router-outlet></router-outlet>
   `,
   styles: ``
 })
