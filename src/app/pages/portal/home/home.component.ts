93d6ae76a1d558fccdb0fcd6fff41358e111ecb9
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-05 14:39:04 -0500

    Limpieza del componente

--- src/app/pages/portal/home/home.component.ts
+++ src/app/pages/portal/home/home.component.ts
@@ -5,18 +5,8 @@ import { AuthService } from '../../../services/auth.service';
   selector: 'app-home',
   imports: [],
   template: `
-    <div class="w-full h-screen flex flex-col items-center justify-center">
-      <button (click)="logOut()" class="bg-red-600 hover:bg-opacity-90 text-white border rounded-full px-4 py-2 outline-none">
-        Cerrar Sesión
-      </button>
-    </div>
+    <p>Home Component Works!</p>
   `,
-  styles: ``
+  styles: ``,
 })
-export class HomeComponent {
-  constructor(private authService: AuthService) {}
-
-  logOut() {
-    this.authService.logOut();
-  }
-}
+export class HomeComponent {}
