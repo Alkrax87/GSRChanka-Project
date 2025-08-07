89498eafaf38f41a60ec6f002524019d560f278f
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-07 14:46:34 -0500

    Se eliminó elementos innecesarios

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBell, faGear, faWindowMaximize } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faGear } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-topbar',
   imports: [FontAwesomeModule],
   template: `
     <div class="h-16 w-full bg-neutral-100 shadow-md text-lg flex items-center pl-2 pr-4 py-2 flex-shrink-0 sticky top-0 z-30">
-      <div class="flex items-center justify-center -rotate-90 cursor-pointer hover:bg-neutral-200 rounded-lg w-7 h-7">
-        <fa-icon [icon]="Toggle" class="text-neutral-700"></fa-icon>
-      </div>
       <div class="w-full flex justify-end gap-2">
         <div class="relative flex items-center justify-center gap-1 cursor-pointer hover:bg-neutral-200 rounded-lg w-fit h-7 px-2">
           @if (notificationsCount) {
@@ -30,7 +27,6 @@ import { faBell, faGear, faWindowMaximize } from '@fortawesome/free-solid-svg-ic
 export class TopbarComponent {
   notificationsCount = 28;
 
-  Toggle = faWindowMaximize;
   Notifications = faBell;
   Configuration = faGear;
 }
