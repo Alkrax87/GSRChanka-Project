9048d04c1f323213e41dddedef1833e07fc19153
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-08 12:55:55 -0500

    Integración de estado del sidebar para agregar responsibidad

--- src/app/pages/portal/main/main.component.ts
+++ src/app/pages/portal/main/main.component.ts
@@ -1,23 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from "../../../components/sidebar/sidebar.component";
-import { TopbarComponent } from "../../../components/topbar/topbar.component";
+import { SidebarComponent } from '../../../components/sidebar/sidebar.component';
+import { TopbarComponent } from '../../../components/topbar/topbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   imports: [CommonModule, SidebarComponent, TopbarComponent, RouterOutlet],
   template: `
-  <div class="flex h-screen overflow-hidden">
-    <app-sidebar></app-sidebar>
-    <div class="flex flex-col flex-1 ml-64 h-full">
-      <app-topbar></app-topbar>
-      <div class="flex-1 overflow-y-auto">
-       <router-outlet></router-outlet>
+    <div class="flex h-screen">
+      <app-sidebar (sidebarStatus)="changeSideBarStatus($event)"></app-sidebar>
+      <div class="w-full duration-300" [ngClass]="{ 'pl-64': sidebarIsOpen, 'pl-12': !sidebarIsOpen }">
+        <app-topbar></app-topbar>
+        <router-outlet></router-outlet>
       </div>
     </div>
-  </div>
   `,
   styles: ``,
 })
-export class MainComponent {}
+export class MainComponent {
+  sidebarIsOpen: boolean = true;
+
+  changeSideBarStatus(status: boolean) {
+    this.sidebarIsOpen = status;
+  }
+}
