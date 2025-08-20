524cabdabdd751b4bd322766b9446618c91ed4bf
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-08-20 12:20:16 -0500

    Se agregó servicio roles para gestionar sus médotodos

---
+++ src/app/services/roles.service.ts
@@ -0,0 +1,38 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { map, Observable } from 'rxjs';
+import { Rol } from '../interfaces/rol';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RolesService {
+  private firestore = inject(Firestore);
+  rolesCollection = collection(this.firestore, 'roles');
+
+  getRoles(): Observable<Rol[]> {
+    return collectionData(this.rolesCollection, { idField: 'id' }) as Observable<Rol[]>;
+  }
+
+  addRol(rol: Rol) {
+    return addDoc(this.rolesCollection, rol);
+  }
+
+  updateRol(id: string, rol: Partial<Rol>) {
+    const rolDoc = doc(this.firestore, `roles/${id}`);
+    return updateDoc(rolDoc, rol);
+  }
+
+  deleteRol(id: string) {
+    const rolDoc = doc(this.firestore, `roles/${id}`);
+    return deleteDoc(rolDoc);
+  }
+}
