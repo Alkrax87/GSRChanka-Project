78ff67f880a11a570f357b29d130f972cddd430f
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-09-22 22:16:58 -0500

    Mejora del componente de tabla: se actualizó el diseño de búsqueda y selección de elementos, se optimizó la paginación y se mejoró la accesibilidad de los botones de acción

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,131 +1,117 @@
 import { Component, EventEmitter, Input, Output, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCaretLeft, faCaretRight, faEdit, faEye, faTrash} from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronLeft, faChevronRight, faChevronUp, faEdit, faEye, faSearch, faTrash} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-table',
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   template: `
-    <!-- Barra de búsqueda -->
-    <div class="btn btn-success mb-4"style="margin-bottom: 10px; text-align: right; width: 100%;">
-      Buscar:&nbsp;
-      <input 
-        type="text" 
-        placeholder="Buscar..." 
-        [value]="searchTerm"
-        (input)="onSearch($event)"
-        style="padding: 6px 10px; border: 1px solid #ccc; border-radius: 4px; outline: none; width: 200px;"
-      />
+    <!-- Seach and Selector -->
+    <div class="flex justify-between">
+      <!-- Search -->
+      <div class="relative w-full md:w-1/2 lg:w-1/3">
+        <fa-icon [icon]="Search" class="absolute left-3 top-1/2 -translate-y-1/2 text-neutral-400"></fa-icon>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          [value]="searchTerm"
+          (input)="onSearch($event)"
+          class="w-full rounded-full shadow-md pl-10 pr-4 py-2 outline-none ring-[3px] ring-transparent focus:ring-main/50 focus:text-main"
+        >
+      </div>
+      <!-- Items -->
+      <div class="flex items-center gap-2">
+        <p class="text-neutral-400 text-sm">Elementos por página:</p>
+        <select
+          [value]="pageSize"
+          (change)="onPageSizeChange($event)"
+          class="bg-white ring-transparent ring-[3px] hover:ring-main/50 text-main text-end text-sm px-3 py-1 rounded-full outline-none cursor-pointer shadow-md"
+        >
+          <option class="text-start" value="10">10</option>
+          <option class="text-start" value="20">20</option>
+          <option class="text-start" value="30">30</option>
+        </select>
+      </div>
     </div>
 
-    <!-- Selector de filas por página -->
-    <div style="margin-bottom: 10px;">
-      <label for="pageSize">Mostrar: </label>
-      <select  id="pageSize" [value]="pageSize" (change)="onPageSizeChange($event)" class="border border-gray-500 rounded-md">
-        <option value="5">5</option>
-        <option value="10">10</option>
-        <option value="15">15</option>
-        
-      </select>
-      &nbsp; &nbsp;<label for="pageSize">Registro por Pagina: </label>
-    </div>
-
-    <!-- Tabla -->
-    <table>
-      <thead>
-        <tr>
-          @for (header of headers; track $index) {
-            <th (click)="sortData(header)" style="cursor: pointer;">
-              {{ header }}
-              <span *ngIf="sortColumn === header">
-                {{ sortDirection === 'asc' ? '▲' : '▼' }}
-              </span>
-            </th>
-          }
-          <th style="text-align: center;">Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        @for (row of paginatedData; track $index) {
-          <tr>
+    <!-- Table -->
+    <div class="my-6 overflow-x-auto shadow-md rounded-3xl border b">
+      <table class="w-full">
+        <thead class="bg-main text-white">
+          <tr class="h-12">
             @for (header of headers; track $index) {
-              <td>{{ row[header] }}</td>
+              <th (click)="sortData(header)" class="cursor-pointer text-start px-3">
+                {{ header }}
+                @if (sortColumn === header) {
+                  @if (sortDirection === 'asc') {
+                    <fa-icon class="text-sm m-0.5" [icon]="Ascendent"></fa-icon>
+                  } @else {
+                    <fa-icon class="text-sm m-0.5" [icon]="Descendent"></fa-icon>
+                  }
+                }
+              </th>
             }
-            <td style="text-align: center;">
-              <button (click)="onShow.emit(row)" title="Ver" class="action-btn view">
-                <fa-icon [icon]="Eye"></fa-icon>
-              </button>
-              <button (click)="onEdit.emit(row)" title="Editar" class="action-btn edit">
-                <fa-icon [icon]="Edit"></fa-icon>
-              </button>
-              <button (click)="onDelete.emit(row)" title="Eliminar" class="action-btn delete">
-                <fa-icon [icon]="Trash"></fa-icon>
-              </button>
-            </td>
+            <th>Opciones</th>
           </tr>
-        }
-      </tbody>
-    </table>
-    <!-- Paginación -->
-    <div style="margin-top: 15px; display: flex; gap: 6px; align-items: center;">
-  <!-- Botón Anterior -->
-  <button class=""
-    (click)="prevPage()" 
-    [disabled]="currentPage === 1"
-    style="padding: 5px 10px; border: 1px solid #ccc; background: white; cursor: pointer;"
-  >
-    <fa-icon [icon]="Left"></fa-icon> Anterior
-  </button>
+        </thead>
+        <tbody>
+          @for (row of paginatedData; track $index) {
+            <tr class="h-10 hover:bg-neutral-100">
+              @for (header of headers; track $index) {
+                <td class="px-3">{{ row[header] }}</td>
+              }
+              <td>
+                <div class="flex items-center justify-center gap-4">
+                  <button (click)="onShow.emit(row)" class="text-main" title="Ver">
+                    <fa-icon [icon]="Eye"></fa-icon>
+                  </button>
+                  <button (click)="onEdit.emit(row)" class="text-yellow-400" title="Editar">
+                    <fa-icon [icon]="Edit"></fa-icon>
+                  </button>
+                  <button (click)="onDelete.emit(row)"  class="text-red-600"title="Eliminar">
+                    <fa-icon [icon]="Trash"></fa-icon>
+                  </button>
+                </div>
+              </td>
+            </tr>
+          }
+        </tbody>
+      </table>
+    </div>
 
-  <!-- Botones de página -->
-  <div style="display: flex; gap: 4px;">
-    @for (page of [].constructor(totalPages); let i = $index; track $index) {
-      <button
-        (click)="goToPage(i + 1)"
-        [style.background]="currentPage === (i+1) ? '#007bff' : 'white'"
-        [style.color]="currentPage === (i+1) ? 'white' : 'black'"
-        style="padding: 5px 10px; border: 1px solid #ccc; cursor: pointer;"
-      >
-        {{ i + 1 }}
-      </button>
-    }
-  </div>
-
-  <!-- Botón Siguiente -->
-    <button 
-      (click)="nextPage()" 
-      [disabled]="currentPage === totalPages"
-      style="padding: 5px 10px; border: 1px solid #ccc; background: white; cursor: pointer;"
-    >
-      Siguiente <fa-icon [icon]="Rigth"></fa-icon>
-    </button>
-  </div>
-  <!-- Texto tipo DataTables -->
-  <div style="font-size: 13px; color: #555;">
-    Mostrando {{ startRecord + 1 }} a {{ endRecord }} de {{ filteredData.length }} registros
-  </div>
-
+    <!-- Paginate -->
+    <div class="flex justify-between gap-2">
+      <!-- Paginate -->
+      <div class="text-neutral-400 text-sm">
+        Mostrando {{ startRecord + 1 }} a {{ endRecord }} de {{ filteredData.length }} registros
+      </div>
+      <!-- Paginate -->
+      <div class="flex gap-2">
+        <!-- Previous -->
+         <button (click)="prevPage()" [disabled]="currentPage === 1" [ngClass]="{'bg-neutral-200 hover:bg-neutral-200': currentPage === 1}" class="bg-main hover:bg-main-hover w-8 h-8 text-white rounded-full font-semibold text-sm">
+          <fa-icon class="text-sm" [icon]="Previous"></fa-icon>
+        </button>
+        <!-- Pages -->
+        <div class="flex gap-0.5">
+          @for (page of [].constructor(totalPages); let i = $index; track $index) {
+            <button (click)="goToPage(i + 1)"
+              [ngClass]="{'bg-main text-white': currentPage === (i+1)}"
+              class="border w-8 h-8 rounded-full hover:bg-main hover:text-white duration-300"
+            >
+              {{ i + 1 }}
+            </button>
+          }
+        </div>
+        <!-- Next -->
+        <button (click)="nextPage()" [disabled]="currentPage === totalPages" [ngClass]="{'bg-neutral-200 hover:bg-neutral-200': currentPage === totalPages}" class="bg-main hover:bg-main-hover w-8 h-8 text-white rounded-full font-semibold text-sm">
+          <fa-icon class="text-sm" [icon]="Next"></fa-icon>
+        </button>
+      </div>
+    </div>
   `,
-  styles: [`
-    table {
-      width: 100%;
-      border-collapse: collapse;
-    }
-    th {
-      background: #f4f4f4;
-      text-align: left;
-      user-select: none;
-    }
-    td, th {
-      border: 1px solid #ddd;
-      padding: 8px;
-    }
-    tr:hover {
-      background: #f9f9f9;
-    }
-  `]
+  styles: ``,
 })
 export class TableComponent implements OnInit, OnChanges{
   @Input() headers: string[] = [];
@@ -134,19 +120,22 @@ export class TableComponent implements OnInit, OnChanges{
   @Output() onShow = new EventEmitter<any>();
   @Output() onEdit = new EventEmitter<any>();
   @Output() onDelete = new EventEmitter<any>();
-  
+
   searchTerm: string = '';
   sortColumn: string = '';
   sortDirection: 'asc' | 'desc' = 'asc';
 
-  Rigth = faCaretRight;
-  Left = faCaretLeft;
+  Ascendent = faChevronUp;
+  Descendent = faChevronDown;
+  Previous = faChevronLeft;
+  Next = faChevronRight;
+  Search = faSearch;
   Eye = faEye;
   Edit = faEdit;
   Trash = faTrash;
 
   currentPage = 1;
-  pageSize = 5;
+  pageSize = 10;
   totalPages = 1;
 
   startRecord = 0;
