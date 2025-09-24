231af18d78ab935b4c06de3a87586babe5b36350
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-09-24 09:57:05 -0500

    Se agregó servicio usuarios para gestionar sus métodos

---
+++ src/app/services/usuarios.service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuariosService {
+  private firestore = inject(Firestore);
+  usersCollection = collection(this.firestore, 'usuarios');
+
+  getUsers(): Observable<Usuario[]> {
+    return collectionData(this.usersCollection, { idField: 'id' }) as Observable<Usuario[]>;
+  }
+
+  addUser(user: Usuario) {
+    return addDoc(this.usersCollection, user);
+  }
+
+  updateUser(id: string, user: Partial<Usuario>) {
+    const userDoc= doc(this.firestore, `usuarios/${id}`);
+    return updateDoc(userDoc, user);
+  }
+
+  deleteUser(id: string) {
+    const userDoc = doc(this.firestore, `usuarios/${id}`);
+    return deleteDoc(userDoc);
+  }
+}
