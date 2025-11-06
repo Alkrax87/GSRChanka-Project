81961bbbb248ff14f10dbc3143a6ed9937177bb4
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-11-06 09:13:40 -0500

    Se implementó observable para obtener los datos del usuario logeado

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,21 +1,37 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc, deleteDoc, doc, updateDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData, deleteDoc, doc, updateDoc, setDoc, getDoc } from '@angular/fire/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Usuario } from '../interfaces/usuario';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsuariosService {
+  private authService = inject(AuthService);
   private firestore = inject(Firestore);
   usersCollection = collection(this.firestore, 'usuarios');
+
+  private usuarioSubject = new BehaviorSubject<Usuario | null>(null);
+  dataUsuario$ = this.usuarioSubject.asObservable();
+
+  async getUserLoggedData() {
+    this.usuarioSubject.next(null);
+    const uid = this.authService.getUserUid();
+
+    if (!uid) return;
+
+    const snapshot = await getDoc(doc(this.firestore, `usuarios/${uid}`));
+    this.usuarioSubject.next(snapshot.data() as Usuario);
+  }
 
   getUsers(): Observable<Usuario[]> {
     return collectionData(this.usersCollection, { idField: 'id' }) as Observable<Usuario[]>;
   }
 
   addUser(user: Usuario) {
-    return addDoc(this.usersCollection, user);
+    const userDoc = doc(this.usersCollection, user.id);
+    return setDoc(userDoc, user);
   }
 
   updateUser(id: string, user: Partial<Usuario>) {
