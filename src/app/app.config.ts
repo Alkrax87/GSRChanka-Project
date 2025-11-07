b32f05c5d2b7e56b04cc39e2f424fe0465a17dc9
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-11-07 14:00:22 -0500

    Se agregó soporte para la localización en español en la configuración de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -8,11 +8,15 @@ import { getStorage, provideStorage } from "@angular/fire/storage";
 import { environment } from './environments';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+registerLocaleData(localeEs)
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'es' },
     provideHttpClient(withFetch()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
