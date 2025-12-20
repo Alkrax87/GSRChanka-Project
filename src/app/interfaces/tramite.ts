6e7bec6556e6d209b4d39554ea272e035e9ced68
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2025-12-20 13:32:50 -0500

    Refactor Tramite interface to simplify structure and enhance clarity by removing unnecessary properties and adding Documento and Movimiento interfaces

--- src/app/interfaces/tramite.ts
+++ src/app/interfaces/tramite.ts
@@ -1,37 +1,28 @@
+export interface Documento {
+  id: string;
+  nombre: string;
+  tipo: 'Revisión' | 'Oficio' | 'Documento' | 'TdR' | 'Otro';
+  adjuntadoPorArea: string;
+  estado: 'Pendiente' | 'Aprobado' | 'Rechazado';
+  rutaArchivo: string;
+}
+
+export interface Movimiento {
+  areaOrigen: string;
+  areaDestino: string;
+  fechaIngreso: Date;
+  fechaSalida: Date | null;
+  prioridad: 'Sin Determinar' | 'Baja' | 'Media' | 'Alta';
+  estado: 'Pendiente' | 'En Proceso' | 'Completado' | 'Cancelado';
+  observaciones?: string;
+}
+
 export interface Tramite {
   id?: string;
   nombre: string;
-  correspondencia: {
-    area: string;
-    actual: {
-      remitente: string;
-      destinatario: string;
-      asunto: string;
-      numeroDocumento: string;
-      tipo: 'Revisión' | 'Oficio' | 'Documento' | 'TdR' | 'Otro';
-    };
-    previo: {
-      remitente: string;
-      destinatario: string;
-      asunto: string;
-      numeroDocumento: string;
-      tipo: 'Revisión' | 'Oficio' | 'Documento' | 'TdR' | 'Otro';
-    };
-  };
-  origen: {
-    area: string;
-    estado: 'Pendiente' | 'En Proceso' | 'Completado' | 'Cancelado';
-    fecha: string;
-  }
-  ubicacionActual: {
-    area: string;
-    fechaIngreso: string;
-    estado: 'Pendiente' | 'En Proceso' | 'Completado' | 'Cancelado';
-  }
-  trazabilidadAreas: {
-    area: string;
-    fechaIngreso: string;
-    fechaSalida?: string;
-    estado: 'Pendiente' | 'En Proceso' | 'Completado' | 'Cancelado';
-  }[];
+  documentos: Documento[];
+  trazabilidad: Movimiento[];
+  estadoGlobal: 'Pendiente' | 'En Proceso' | 'Completado' | 'Cancelado';
+  areaActual: string;
+  areaCreacion: string;
 }
