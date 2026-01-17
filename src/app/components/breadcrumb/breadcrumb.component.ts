5df56626834cc54c6fb95a2cd8a94977271ef3a0
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-01-17 16:02:54 -0500

    Se agregó breadcrumb para visualización de ubicación

---
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleRight, faHouse } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-breadcrumb',
+  imports: [FontAwesomeModule],
+  template: `
+    <div class="text-main flex gap-2 font-semibold text-sm">
+      <fa-icon [icon]="Home"></fa-icon>
+      <fa-icon [icon]="Arrow"></fa-icon>
+      <span>{{ path }}</span>
+    </div>
+  `,
+  styles: ``,
+})
+export class BreadcrumbComponent {
+  @Input() path!: string;
+
+  Home = faHouse;
+  Arrow = faAngleRight;
+}
