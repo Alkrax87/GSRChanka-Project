17f177816228ace32e2614e8042880e27dad05b8
Alkrax87 <95942732+Alkrax87@users.noreply.github.com>
2026-08-26 22:57:09 -0500

    Se creó la interfaz AuthUser y se actualizó el servicio de autenticación para manejar el estado del usuario con la nueva estructura de datos

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,16 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { Auth, browserSessionPersistence, createUserWithEmailAndPassword, onAuthStateChanged, setPersistence, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { Auth, browserSessionPersistence, onAuthStateChanged, setPersistence, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 import { Router } from '@angular/router';
-import { Usuario } from '../interfaces/usuario';
-import { doc, Firestore, getDoc } from '@angular/fire/firestore';
+import { AuthUser } from '../interfaces/auth-user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private auth = inject(Auth);
-  private firestore = inject(Firestore);
   private router = inject(Router);
 
-  private _usuariologged = signal<Usuario | null>(null);
+  private _usuariologged = signal<AuthUser | null>(null);
   public usuarioLogged = this._usuariologged.asReadonly();
 
   constructor() {
@@ -22,29 +20,19 @@ export class AuthService {
   private listenAuthChanges() {
     onAuthStateChanged(this.auth, async (user) => {
       if (user) {
-        this.loadUserData(user.uid);
+        const claims: any = (await user.getIdTokenResult(true)).claims;
+
+        this._usuariologged.set({
+          uid: user.uid,
+          username: user.email!.split('@')[0],
+          displayName: user.displayName!,
+          role: claims['rol'],
+          dependenciaId: claims['dependenciaId'],
+        });
       } else {
         this._usuariologged.set(null);
       }
     });
-  }
-
-  private async loadUserData(uid: string) {
-    const userRef = doc(this.firestore, `usuarios/${uid}`);
-    const snapshot = await getDoc(userRef);
-
-    if (snapshot.exists()) {
-      this._usuariologged.set({
-        id: snapshot.id,
-        ...snapshot.data(),
-      } as Usuario);
-    } else {
-      this._usuariologged.set(null);
-    }
-  }
-
-  registerUser(email: string, password: string) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
   async login(email: string, password: string) {
